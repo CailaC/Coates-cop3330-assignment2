3abde39b8575ab1cfd9255696d7a867f5e97e4aa
CailaC <caila.coates2010@gmail.com>
2021-09-27 18:01:33 -0400

    Commit 1

--- src/main/java/ex35/ex35Main.java
+++ src/main/java/ex35/ex35Main.java
@@ -1,4 +1,49 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Caila Coates
+ */
 package ex35;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class ex35Main {
+    public static void main(String[] args){
+
+        buildArray();
+
+    }
+
+    public static void buildArray (){
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> entriesArray = new ArrayList<String>();
+
+        question();
+        String name;
+
+        while(true){
+            name = input.nextLine();
+            entriesArray.add(name);
+
+            if(name == ""){
+                selectWinner(entriesArray);
+                break;
+            }
+            question();
+        }
+    }
+
+    public static void question(){
+        System.out.println("Enter a name: ");
+    }
+
+    public static void selectWinner(ArrayList<String> entriesArray){
+        Random arrayNum = new Random();
+        int randomNumber = arrayNum.nextInt(entriesArray.size());
+
+        String randomElement = entriesArray.get(randomNumber);
+        System.out.println("The winner is... " + randomElement + ".");
+    }
+
 }
--- src/main/java/ex37/ex37Main.java
+++ src/main/java/ex37/ex37Main.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Caila Coates
+ */
 package ex37;
 
 public class ex37Main {
